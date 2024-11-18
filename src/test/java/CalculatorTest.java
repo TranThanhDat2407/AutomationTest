1f0d67ec6313324d48fb1beef3b16ed18ac51267
TranThanhDat2407 <tranthanhdat2407.email@gmail.com>
2024-11-18 09:53:32 +0700

    first commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,13 @@
+import org.example.Calculator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(2, calculator.add(1, 1)); // Kiểm tra 1 + 1 = 2
+        Assert.assertEquals(0, calculator.add(1, -1)); // Kiểm tra 1 + (-1) = 0
+        Assert.assertEquals(-2, calculator.add(-1, -1)); // Kiểm tra -1 + (-1) = -2
+    }
+}
